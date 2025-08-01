95550f36302b15cd500ff504f3ff0e2ee73944f7
Kawly_YT <110187169+123araDev@users.noreply.github.com>
2025-08-01 17:29:29 +0100

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,34 +1,19 @@
--- Configuration
-local password = "azerty123"
-local diskSide = "back"
-local modemSide = "left"  -- modem filaire connecté ici
+local modemSide = "left"  -- modem filaire ici
+local redstoneSide = "right"  -- redstone branchée ici
 local channel = 12
 
 -- Ouvre le modem
 rednet.open(modemSide)
 
+print("Attente du signal...")
+
 while true do
-  if disk.isPresent(diskSide) and fs.exists("/disk/ccc.txt") then
-    local f = fs.open("/disk/ccc.txt", "r")
-    local input = f.readAll()
-    f.close()
-
-    input = string.gsub(input, "\n", "") -- nettoyage
-
-    if input == password then
-      print("Mot de passe correct !")
-
-      -- Envoie un message via rednet à tous les ordinateurs sur le canal
-      rednet.broadcast("unlock", channel)
-
-      -- Éjecte le disque
-      disk.eject(diskSide)
-    else
-      print("Mot de passe incorrect.")
-    end
-
-    sleep(1)
-  else
-    sleep(0.5)
+  local id, msg = rednet.receive(channel)
+  if msg == "unlock" then
+    print("Déverrouillage reçu.")
+    redstone.setOutput(redstoneSide, true)
+    os.sleep(0.05)
+    redstone.setOutput(redstoneSide, false)
   end
 end
+
